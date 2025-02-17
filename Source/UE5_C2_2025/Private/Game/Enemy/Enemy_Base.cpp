d743be82ce4b8fe4fda160028f6d3bc6653e7254
nigirimeshi02 <104492636+nigirimeshi02@users.noreply.github.com>
2025-02-17 12:34:24 +0900

    プレイヤー、敵にダメージを与えられるようにした

---
+++ Source/UE5_C2_2025/Private/Game/Enemy/Enemy_Base.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/Enemy/Enemy_Base.h"
+#include "Components/WidgetComponent.h"
+
+// Sets default values
+AEnemy_Base::AEnemy_Base()
+{
+ 	//Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//WidgetComponentを追加する
+	LockOnMarkerWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("WidgetComponent"));
+	LockOnMarkerWidget->SetupAttachment(RootComponent);
+	LockOnMarkerWidget->SetWidgetSpace(EWidgetSpace::Screen);
+	//設定したいウィジェットのクラスを作成
+	FString LockOnMarkerWidgetPath = TEXT("/Game/Game/UI/WBP_LockOn.WBP_LockOn_C");
+	TSubclassOf<UUserWidget>LockOnMarkerWidgetClass = TSoftClassPtr<UUserWidget>(FSoftObjectPath(*LockOnMarkerWidgetPath)).LoadSynchronous();
+	//SetWidgetでnullptrを入れないとクラッシュする
+	LockOnMarkerWidget->SetWidget(nullptr);
+	//WidgetClassを設定
+	LockOnMarkerWidget->SetWidgetClass(LockOnMarkerWidgetClass);
+	//非表示にする
+	LockOnMarkerWidget->SetVisibility(false);
+
+	//AbilitySystemコンポーネントを追加する
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+
+	//エネミー用AttributeSetを追加
+	EnemyBaseAttributeSet = CreateDefaultSubobject<UEnemyBaseAttributeSet>(TEXT("AttributeSet"));
+}
+
+// Called when the game starts or when spawned
+void AEnemy_Base::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEnemy_Base::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AEnemy_Base::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AEnemy_Base::SetLockOnEnable_Implementation(bool LockOnFlg)
+{
+	if (LockOnFlg)
+	{
+		LockOnMarkerWidget->SetVisibility(true);
+	}
+	else
+	{
+		LockOnMarkerWidget->SetVisibility(false);
+	}
+}
+
---
+++ Source/UE5_C2_2025/Public/Game/Enemy/EnemyBaseAttributeSet.h
@@ -0,0 +1,88 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "GameplayEffect.h"
+#include "GameplayEffectExtension.h"
+#include "EnemyBaseAttributeSet.generated.h"
+
+//アクセサ(Setter、Getter)を生成するためのマクロ
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_C2_2025_API UEnemyBaseAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	//コンストラクタ
+	UEnemyBaseAttributeSet();
+
+	// GameplayEffect動作後の処理
+	void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)override;
+	
+	//体力
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UEnemyBaseAttributeSet, Health)
+
+	//最大体力
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UEnemyBaseAttributeSet, MaxHealth)
+
+	//スタミナ
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData Stamina;
+	ATTRIBUTE_ACCESSORS(UEnemyBaseAttributeSet, Stamina)
+
+	//最大スタミナ
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData MaxStamina;
+	ATTRIBUTE_ACCESSORS(UEnemyBaseAttributeSet, MaxStamina)
+
+	//エネルギー
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData Energy;
+	ATTRIBUTE_ACCESSORS(UEnemyBaseAttributeSet, Energy)
+
+	//最大エネルギー
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData MaxEnergy;
+	ATTRIBUTE_ACCESSORS(UEnemyBaseAttributeSet, MaxEnergy)
+
+	//攻撃力
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(UEnemyBaseAttributeSet, AttackPower)
+
+	//防御力
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS(UEnemyBaseAttributeSet, DefensePower)
+
+	//クリティカル率
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData CriticalRate;
+	ATTRIBUTE_ACCESSORS(UEnemyBaseAttributeSet, CriticalRate)
+
+	//クリティカルダメージ
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData CriticalDamage;
+	ATTRIBUTE_ACCESSORS(UEnemyBaseAttributeSet, CriticalDamage)
+
+	//攻撃速度
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttributeData AttackSpeed;
+	ATTRIBUTE_ACCESSORS(UEnemyBaseAttributeSet, AttackSpeed)
+};
